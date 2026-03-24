5c76d923040ba1f51b9ac7801818f1bfb26758ff
Deepansh Vishwakarma <117040777+codebydeep@users.noreply.github.com>
2026-03-25 02:58:30 +0530

    Update heading text on Homepage component

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -15,7 +15,7 @@ const Homepage = () => {
   <div className="hero-overlay"></div>
   <div className="hero-content text-neutral-content text-center">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">New Way to Start your Coding Journey</h1>
+      <h1 className="mb-5 text-5xl font-bold">New Way to Start Coding Journey</h1>
       <p className="mb-5">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
